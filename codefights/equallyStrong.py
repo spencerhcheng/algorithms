d06bb6d95c6878ae5d27eb0035a5fbf8ef3cb1a8
spencerhcheng <spencerhcheng@gmail.com>
2018-04-06 06:05:04 -0700

    equallyStrong: initial commit

---
+++ codefights/equallyStrong.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+"""
+Call two arms equally strong if the heaviest weights they each are able to lift are equal.
+
+Call two people equally strong if their strongest arms are equally strong (the strongest arm can be both the right and the left), and so are their weakest arms.
+
+Given your and your friend's arms' lifting capabilities find out if you two are equally strong.
+
+Example
+
+For yourLeft = 10, yourRight = 15, friendsLeft = 15 and friendsRight = 10, the output should be
+areEquallyStrong(yourLeft, yourRight, friendsLeft, friendsRight) = true;
+For yourLeft = 15, yourRight = 10, friendsLeft = 15 and friendsRight = 10, the output should be
+areEquallyStrong(yourLeft, yourRight, friendsLeft, friendsRight) = true;
+For yourLeft = 15, yourRight = 10, friendsLeft = 15 and friendsRight = 9, the output should be
+areEquallyStrong(yourLeft, yourRight, friendsLeft, friendsRight) = false.
+"""
+
+def areEquallyStrong(yourLeft, yourRight, friendsLeft, friendsRight):
+    """
+    Strongest arm of self or friend can either be left or right.
+    Thus, any matching pair of max and min will return True
+    """
+    if max(yourLeft, yourRight) == max(friendsLeft, friendsRight) and min(yourLeft, yourRight) == min(friendsLeft, friendsRight):
+        return True
+    return False
+
+== "__main__":
+    yourLeft = 10
+    yourRight = 15
+    friendsLeft = 15
+    friendsRight = 10
+    print(areEquallyStrong(yourLeft, yourRight, friendsLeft, friendsRight))
